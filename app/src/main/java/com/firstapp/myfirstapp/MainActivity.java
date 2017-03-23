9d2825e9ba5f3ef1d9175b0c3f6f15595bcff27b
linwenhan <linwenhan4566@163.com>
2017-03-24 00:36:24 +0800

    了解了下布局，intent互调activity，finish结束活动，按钮监听

--- app/src/main/java/com/firstapp/myfirstapp/MainActivity.java
+++ app/src/main/java/com/firstapp/myfirstapp/MainActivity.java
@@ -1,13 +1,56 @@
 package com.firstapp.myfirstapp;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        Button button1 =(Button) findViewById(R.id.button1);
+        Button button2 =(Button) findViewById(R.id.button2);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this,"你点击了我，但我不去下一个页面",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+             finish();
+
+            }
+        });
     }
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(this, "You clicked Add", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "You clicked Remove", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+            return true;
+        }
+
 }
